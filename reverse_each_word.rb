f0f75b5cb556e67bd099f815e5f5b5e4b60959c3
Kyle Luke <th.oldschool@gmail.com>
2019-07-30 04:51:36 +0000

    Automatically backed up by Learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,10 @@
+def reverse_each_word(string)
+  string_array = []
+  string_array = string.split(" ")
+  reversed = []
+  reversed = string_array.each{|n| n.split("").reverse.join("")}
+  reversed.join(' ')
+  
+  return reversed
+end
+puts reverse_each_word("hello")
